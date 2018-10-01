3634a9cb2dc16787fab16b361a4e1ec1dc307d33
Zetok Zalbavar <zexavexxe@gmail.com>
2018-10-01 18:27:14 +0100

    Refactor out python2.7 support in the buildsite.py script

--- buildsite.py
+++ buildsite.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
-from __future__ import print_function
+
+import json
 import os
 import pystache
 import re
 import shutil
-import sys
-
-try:
-    import simplejson
-except ImportError:
-    import json as simplejson
 
 AUTHORITATIVE = "en"
 
-def do_format_template(name, template):
+def do_format_template(name: str, template: bytes):
     basename = ".".join(name.split(".")[:-1])
     if not basename:
         return
@@ -21,23 +16,27 @@ def do_format_template(name, template):
     language_files = [lf for lf in os.listdir(os.getcwd())
                       if lf.endswith(".json") and lf.startswith(basename + ".")]
     print("{0} found.".format(len(language_files)))
-    print("Using {0}.{1}.json as a base.".format(basename, AUTHORITATIVE))
 
-    with open("{0}.{1}.json".format(basename, AUTHORITATIVE), "r") as af:
-        main_values = simplejson.load(af)
+    af_fname = "{0}.{1}.json".format(basename, AUTHORITATIVE)
+    print("Using {} as a base.".format(af_fname))
+
+    with open(af_fname, "r") as af:
+        main_values = json.load(af)
 
     renderer = pystache.renderer.Renderer()
     lang_values = {}
     lang_list = []
     for language in language_files:
         language = language.split(".")[-2]
+        lf_fname = "{0}.{1}.json".format(basename, language)
+
         if not re.match(r"^[A-Za-z0-9_.]+$", language):
             continue
-        with open("{0}.{1}.json".format(basename, language), "r") as lf:
+        with open(lf_fname, "r") as lf:
             try:
-                values = simplejson.load(lf)
+                values = json.load(lf)
             except ValueError:
-                print("Error in {0}.{1}.json!".format(basename, language))
+                print("Error in {}!".format(lf_fname))
                 raise
         for key in values:
             if isinstance(values[key], str):
@@ -53,12 +52,8 @@ def do_format_template(name, template):
     for language in language_files:
         language = language.split(".")[-2]
         with open("site/{1}.html".format(basename, language), "w") as outfile:
-            if sys.version_info.major == 2:
-                outfile.write(renderer.render(template, lang_values[language],
-                              languages=lang_list).encode("utf-8"))
-            else:
-                outfile.write(renderer.render(template, lang_values[language],
-                              languages=lang_list))
+            outfile.write(renderer.render(template, lang_values[language],
+                          languages=lang_list))
 
 def main():
     if not os.path.isdir(os.path.join(os.getcwd(), "site")):
@@ -66,15 +61,12 @@ def main():
     if os.path.isdir(os.path.join("site", "assets")):
         shutil.rmtree(os.path.join("site", "assets"))
     shutil.copytree("assets", os.path.join("site", "assets"))
-    templates = [tp_name for tp_name in os.listdir(os.getcwd())
+    templates = [tp_name for tp_name in os.listdir()
                  if tp_name.endswith(".mustache")]
     for template_name in templates:
         with open(template_name) as tmp_file:
             print("Building template {0}.".format(template_name))
-            if sys.version_info.major == 2:
-                do_format_template(template_name, tmp_file.read().decode("utf-8"))
-            else:
-                do_format_template(template_name, tmp_file.read())
+            do_format_template(template_name, tmp_file.read())
 
 if __name__ == "__main__":
     main()
