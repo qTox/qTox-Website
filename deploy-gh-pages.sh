b8f696f38446d1da760bd82948271dce68eb1ab8
Anthony Bilinski <me@abilinski.com>
2022-02-14 03:42:14 -0800

    Grant action push permission to qtox.github.io.git through a deploy key

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# used in travis to deploy documentation to github pages
+# used in gh actions to deploy documentation to github pages
 
 
 cd site/
 cp en.html index.html
 
 git init
-git config user.name "Travis CI"
+git config user.name "qTox bot"
 git config user.email "qTox@users.noreply.github.com"
 git add .
 git commit -m "Deploy to GH pages."
-git push --force --quiet "https://${GH_TOKEN}@github.com/qTox/qtox.github.io.git" master:master &> /dev/null
+echo "$access_key" > /tmp/access_key
+GIT_SSH_COMMAND="ssh -i /tmp/access_key" git push --force --quiet "git@github.com:qTox/qtox.github.io.git" master:master &> /dev/null
